3d7bcc97a5574b321912c824380c0f15064961b2
spomega <spomesys@gmail.com>
2024-12-12 09:48:27 +0100

    updates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,22 +2,36 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = ["Hello New Beginning","hi"];
 
     #[Route('/{limit<\d+>?3}', name: 'index')]
     public function index(int $limit): Response
     {
-        return new Response(implode(",", array_splice($this->messages, 0, $limit)));
+        //return new Response(implode(",", array_splice($this->messages, 0, $limit)));
+
+       return $this->render(
+            'hello/index.html.twig',
+            [
+                'message' => implode(",", array_splice($this->messages, 0, $limit))
+            ]
+        );
     }
 
     #[Route('/messages/{id<\d+>}', name: 'messages', methods: ['GET'])]
     public function showOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        $message = $id > count($this->messages)-1 ? Sprintf("Message %d not found.", $id) : $this->messages[$id];
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $message,
+            ]
+        );
     }
 }
